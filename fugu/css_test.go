f7b351e939ca487d4b93ba36ecacc88f5c40a95f
Stephan Schmidt <stephan@svese.me>
2022-09-26 11:23:15 +0200

    Fix test

--- fugu/css_test.go
+++ fugu/css_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestCss(t *testing.T) {
 	css := "   @import url('https://fonts.googleapis.com/css?family=Muli')    "
-	imports := ImportsFromCss(css)
+	imports := ImportsFromCss(css, false)
 
 	assert.Equal(t, 1, len(imports))
 	assert.Equal(t, "https://fonts.googleapis.com/css?family=Muli", imports[0])
